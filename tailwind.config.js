1aa9ba566b2d8db86f26b0f816333c9e26addb14
pivnati <natanmuleta911@gmail.com>
2026-04-18 09:57:53 -1200

    add the sign up page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-      "./App.tsx",
-      "./app/**/*.{js,jsx,ts,tsx}",
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./components/**/*.{js,jsx,ts,tsx}",
-    ],
-    presets: [require("nativewind/preset")],
-    theme: {
-      extend: {
-        colors: {
-          brand: {
-            50: "#edf7f1",
-            100: "#d0eedd",
-            200: "#a4ddbc",
-            300: "#78cb9a",
-            400: "#50b97d",
-            500: "#329f61",
-            600: "#237e4b",
-            700: "#185f38",
-            800: "#0f3f25",
-            900: "#082112",
-          },
-        },
-      },
-    },
-
-    plugins: [],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./App.tsx",
+      "./app/**/*.{js,jsx,ts,tsx}",
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
+    ],
+    presets: [require("nativewind/preset")],
+    theme: {
+      extend: {
+        colors: {
+          brand: {
+            50: "#edf7f1",
+            100: "#d0eedd",
+            200: "#a4ddbc",
+            300: "#78cb9a",
+            400: "#50b97d",
+            500: "#329f61",
+            600: "#237e4b",
+            700: "#185f38",
+            800: "#0f3f25",
+            900: "#082112",
+          },
+        },
+      },
+    },
+
+    plugins: [],
   };
+
